ccc07289a6a08aac266a2fee51f84600631c9f37
Moritz Beyreuther <random.seed@web.de>
2011-04-17 13:17:10 +0000

    Stubs for mopad wrapper

---
+++ branches/obspy.imaging/core.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#-----------------------------------------------
+# Filename: core.py
+#  Purpose: Wrapper for mopad
+#   Author: Tobias Megies, Moritz Beyreuther
+#    Email: megies@geophysik.uni-muenchen.de
+#
+# Copyright (C) 2008-2011 ObsPy Development Team
+#-----------------------------------------------
+"""
+ObsPy bindings to mopad
+"""
+
+from mopad import Beachball
