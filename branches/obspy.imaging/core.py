4bb207ece9470610260d3bc5fb3294fd792268a7
Tobias Megies <megies@geophysik.uni-muenchen.de>
2011-04-17 13:35:46 +0000

    mopad integration: basic beachball plot from tutorial

--- branches/obspy.imaging/core.py
+++ branches/obspy.imaging/core.py
@@ -11,4 +11,64 @@
 ObsPy bindings to mopad
 """
 
-from mopad import Beachball
+from mopad import BeachBall as mopad_BeachBall
+from mopad import MomentTensor as mopad_MomentTensor
+
+
+def Beach(fm, linewidth=2, facecolor='b', bgcolor='w', edgecolor='k',
+          alpha=1.0, xy=(0, 0), width=200, size=100, nofill=False,
+          zorder=100):
+    """
+    Return a beach ball as a collection which can be connected to an
+    current matplotlib axes instance (ax.add_collection).
+    
+    S1, D1, and R1, the strike, dip and rake of one of the focal planes, can 
+    be vectors of multiple focal mechanisms.
+    
+    :param fm: Focal mechanism that is either number of mechanisms (NM) by 3 
+        (strike, dip, and rake) or NM x 6 (Mxx, Myy, Mzz, Mxy, Mxz, Myz - the 
+        six independent components of the moment tensor). The strike is of the 
+        first plane, clockwise relative to north. 
+        The dip is of the first plane, defined clockwise and perpendicular to 
+        strike, relative to horizontal such that 0 is horizontal and 90 is 
+        vertical. The rake is of the first focal plane solution. 90 moves the 
+        hanging wall up-dip (thrust), 0 moves it in the strike direction 
+        (left-lateral), -90 moves it down-dip (normal), and 180 moves it 
+        opposite to strike (right-lateral). 
+    :param size: Controls the number of interpolation points for the
+        curves. Minimum is automatically set to 100.
+    :param facecolor: Color to use for quadrants of tension; can be a string, e.g. 
+        'r', 'b' or three component color vector, [R G B].
+    :param edgecolor: Color of the edges.
+    :param bgcolor: The background color, usually white.
+    :param alpha: The alpha level of the beach ball.
+    :param xy: Origin position of the beach ball as tuple.
+    :param width: Symbol size of beach ball.
+    :param nofill: Do not fill the beach ball, but only plot the planes.
+    :param zorder: Set zorder. Artists with lower zorder values are drawn
+                   first.
+    """
+    return
+
+def Beachball(fm, size=200, linewidth=2, facecolor='b', edgecolor='k',
+              bgcolor='w', alpha=1.0, xy=(0, 0), width=200, outfile=None,
+              format=None, nofill=False, fig=None):
+    """
+    Draws a beach ball diagram of an earthquake focal mechanism. 
+    
+    S1, D1, and R1, the strike, dip and rake of one of the focal planes, can 
+    be vectors of multiple focal mechanisms.
+
+    :param size: Draw with this diameter.
+    :param fig: Give an existing figure instance to plot into. New Figure if
+                set to None.
+    :param format: If specified the format in which the plot should be
+                   saved. E.g. (pdf, png, jpg, eps)
+
+    For info on the remaining parameters see the
+    :func:`~obspy.imaging.beachball.Beach` function of this module.
+    """
+    mt = mopad_MomentTensor(fm, "USE")
+    bb = mopad_BeachBall(mt)
+    bb.ploBB({})
+
