69e0606220a3d5b248c5086195bbdd12e94c68b7
Tobias Megies <megies@geophysik.uni-muenchen.de>
2011-07-22 11:54:10 +0000

    branches/reftek:
     - sorting packets together by event number instead of year, day-of-year
     - this is how it is done during acquisition, too
     - also adding additional information to output filenames:
       - how many EH/ET/DT packets in event
       - marking as ".bad" if not 1 EH and ET packet

--- branches/megies/reftek_rescue.py
+++ branches/megies/reftek_rescue.py
@@ -26,6 +26,7 @@ Packets are written to one file per day.
 from __future__ import with_statement
 import os
 import mmap
+import warnings
 import contextlib
 from binascii import b2a_hex, a2b_hex
 
@@ -36,7 +37,7 @@ experiment_number = "00" # two chars, usually "00" (?)
 year = "11" # last two digits of year as string
 reftek_id = "A03F" # four char REFTEK DAS ID
 # outfolder should be empty! data might get appended to existing files!
-outfolder = "/export/data/rescue/all_daily_test/"
+outfolder = "/export/data/rescue/all_daily_test2/"
 #==============================================================================
 
 
@@ -49,6 +50,12 @@ PACKET_TYPES = ('DT', 'EH', 'ET')
 # regular expressions module.
 pattern = experiment_number + year + reftek_id
 pattern = a2b_hex(pattern)
+
+msg = "Output directory is expected to be empty. Data might get appended " + \
+      "to existing files"
+warnings.warn(msg)
+
+event_info = {}
 
 # memory map the file
 with open(file_dd, 'r') as f:
@@ -64,17 +71,43 @@ with open(file_dd, 'r') as f:
             # ind marks the actual packet start 2 bytes left of pos
             ind = pos - 2
             # if it seems we have found a packet, process it
-            if m[ind:ind+2] in PACKET_TYPES:
+            pt = m[ind:ind+2]
+            if pt in PACKET_TYPES:
                 # all packet types have the same 16 byte header
                 header = m[ind:ind+16]
                 # from byte 3 onward information is stored in packed BCD format
                 header = header[:2] + b2a_hex(header[2:])
                 header = header.upper()
+                # get event number, this is encoded in 2 bytes after the header
+                # at least for packet types 'DT', 'EH' and 'ET'
+                # one event has one 'EH' and 'ET' and usually many 'DT' packets
+                event_no = int(b2a_hex(m[ind+16:ind+18]))
+                # add event/packettype information to dictionary
+                try:
+                    d = event_info[event_no]
+                except:
+                    d = {'EH': 0, 'ET': 0, 'DT': 0}
+                    event_info[event_no] = d
+                d[pt] += 1
                 # all packets consist of 1024 bytes
                 packet = m[ind:ind+1024]
-                # write to outfolder, one file per day
-                filename = header[2:13] + ".reftek"
+                # write to outfolder, one file per recording event
+                filename = "%s-%04d" % (reftek_id, event_no)
                 filename = os.path.join(outfolder, filename)
                 open(filename, "ab").write(packet)
             # search for pattern in memory map starting right of last position
             pos = m.find(pattern, pos + 1)
+
+# rename event files with packet information included
+for ev_no, ev_info in event_info.iteritems():
+    filename_old = "%s-%04d" % (reftek_id, ev_no)
+    filename_new = filename_old + \
+            ".%d-%d-%05d" % (ev_info['EH'], ev_info['ET'], ev_info['DT'])
+    if ev_info['EH'] != 1 or ev_info['ET'] != 1 or ev_info['DT'] < 1:
+        filename_new += ".bad"
+    else:
+        filename_new += ".ok"
+    filename_new += ".reftek"
+    filename_old = os.path.join(outfolder, filename_old)
+    filename_new = os.path.join(outfolder, filename_new)
+    os.rename(filename_old, filename_new)
