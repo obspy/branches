a181129cf6c4cea33710ea40e2031e1508e24f5b
Yannik Behr <yannikbehr@yanmail.de>
2011-07-28 05:27:19 +0000

    added a few features to data reader

--- branches/strong_motion/read_gns_sm_data.py
+++ branches/strong_motion/read_gns_sm_data.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env python
-
+"""
+A python class to read Volume 1 and Volume 2 formatted strong motion data files
+as used by the strong motion working group at GNS Science, New Zealand.
+"""
 import os
-from obspy.core import Trace, UTCDateTime
+from obspy.core import Trace, UTCDateTime, Stream
 import numpy as np
 
+class Vol12Error(Exception): pass
 
-class Vol1Reader(object):
+class Vol12Reader(object):
     def __init__(self,filename):
         self.fn = filename
-        self.trace = self.read(filename)
-    
-    def read(self,fn):
-        f = open(fn, 'r')
+        self.f = open(self.fn, 'r')
+        self.stream = Stream()
+        for i in xrange(3):
+            self.stream += self.read_comp(self.f)
+        
+    def read_comp(self,f):
         smdict = {}
         f.readline()
         a = f.readline().split()
@@ -25,39 +30,66 @@ class Vol1Reader(object):
         f.readline()
         a = f.readline().split()
         site = a[1]
-        for i in xrange(3): f.readline()
-        npts = int(f.readline().split()[3])
-        delta = float(f.readline().split()[-2])
-        f.readline()
+        for i in xrange(6): f.readline()
         comp = f.readline().split()[1]
         for i in xrange(3): f.readline()
         a = f.readline().split()
         year, month, day, hour, minute, sec = map(int,a[0:6])
         nsec, microsec = divmod(sec,10)
         microsec *= 1e5
-        stats = {'network': '', 'delta': delta,
-                 'station': site, 'location': '',
-                 'starttime': UTCDateTime(year, month, day, hour, minute, nsec, int(microsec)),
-                 'npts': npts, 'calib': 1.0,
-                 'sampling_rate': 1./delta, 'channel': comp,'smdict':smdict}
-        for i in xrange(9): f.readline()
-        cnt = 0
-        data = []
-        while cnt < npts:
-            a = f.readline().split()
-            cnt += len(a)
-            for _e in map(float,a):
-                data.append(_e)
-        
-        data = np.array(data)
-        tr = Trace(data,stats)
-        return tr
+        for i in xrange(2):f.readline()
+        a = map(int,f.readline().split())
+        smdict['prepend'] = a[1]
+        smdict['append'] = a[2]
+        nptsa = a[3]
+        nptsv = a[4]
+        nptsd = a[5]
+        listnpts = [nptsa,nptsv,nptsd]        
+        for i in xrange(2): f.readline()
+        a = map(float,f.readline().split())
+        delta = a[5]
+        for i in xrange(3): f.readline()
+        st = Stream()
+        for i,npts in enumerate(listnpts):
+            if npts < 1: break
+            stats = {'network': '', 'delta': delta,
+                     'station': site, 'location': '',
+                     'starttime': UTCDateTime(year, month, day, hour, minute, nsec, int(microsec)),
+                     'npts': npts, 'calib': 1.0,
+                     'sampling_rate': 1./delta, 'channel': comp,'smdict':smdict}
+            cnt = 0
+            data = []
+            intv = 8
+            while cnt < npts:
+                a = f.readline()
+                a = a.rstrip()
+                old = 0
+                while True:
+                    val = a[old:old+intv]
+                    if not val: break
+                    if float(val) > 999999.:
+                        data.append(0.)
+                    else:
+                        data.append(float(val))
+                    cnt += 1
+                    old += intv
+            if cnt != npts: 
+                raise Vol12Error("Number of samples in header inconsistent with number of samples in trace: %s"%self.fn)
+            data = np.array(data)
+            st.append(Trace(data,stats))
+                
+        return st
 
 if __name__ == '__main__':
-    dirdat = r'H:\workspace\svn.obspy.org\branches\strong_motion'
+    import shutil
+    dirdat = u'H:\workspace\svn.obspy.org\\branches\strong_motion'
     testfn = os.path.join(dirdat, 'O90400A1.V1A')
-    v1 = Vol1Reader(testfn)
-    v1.trace.plot() 
-    print v1.trace.stats    
+    testfn = os.path.join(dirdat,'19900210032700E_Vol2_O90400A1.V2A')
+    #testfn = r's:\proc\2011\06_Prelim\2011-06-13_022049\Vol1\data\20110613_022049_CACS.v1a'
+    testfn = '20110613_022049_CACS.v1a'
+    #shutil.copy(testfn,dirdat)
+    v1 = Vol12Reader(testfn)
+    print v1.stream
+    v1.stream.plot() 
     
         
