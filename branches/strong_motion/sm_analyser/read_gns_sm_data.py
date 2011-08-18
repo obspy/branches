f93bade4406a6cf9bfd8da3ad81aad65b56b9c20
Yannik Behr <yannikbehr@yanmail.de>
2011-08-18 04:52:34 +0000

    check for file existence; add headonly option; read in more meta data

--- branches/strong_motion/sm_analyser/read_gns_sm_data.py
+++ branches/strong_motion/sm_analyser/read_gns_sm_data.py
@@ -15,10 +15,13 @@ class Vol12Reader(object):
     """
     Main class containing the routines to read Volume 1 and Volume 2 records.
     """
-    def __init__(self,filename):
+    def __init__(self,filename,headonly=False):
+        if not os.path.isfile(filename):
+            raise Vol12Error("Can't find file %s."%filename)
         self.fn = filename
         self.f = open(self.fn, 'r')
         self.stream = Stream()
+        self.headonly = headonly
         for i in xrange(3):
             self.stream += self.read_comp(self.f)
         
@@ -28,7 +31,8 @@ class Vol12Reader(object):
         times to read data from all three sensor orientations.
         """
         smdict = {}
-        f.readline()
+        # read alphanumeric header
+        f.readline()#skip one line
         a = f.readline().split()
         latitude = float(a[2])+float(a[3])/60. +float(a[4][0:-1])/(60.*60.)
         longitude = float(a[5])+float(a[6])/60.+float(a[7][0:-1])/(60.*60.)
@@ -37,28 +41,33 @@ class Vol12Reader(object):
         smdict['site'] = a[1]
         smdict['site-name'] = f.readline().rstrip()
         smdict['instrument'] = f.readline().rstrip()
-        f.readline()
+        f.readline()#skip one line
         a = f.readline().split()
         site = a[1]
-        for i in xrange(6): f.readline()
+        for i in xrange(6): f.readline()#skip six lines
         comp = f.readline().split()[1]
-        for i in xrange(3): f.readline()
+        for i in xrange(3): f.readline()#skip three lines
+        # read integer header
         a = f.readline().split()
         year, month, day, hour, minute, sec = map(int,a[0:6])
         nsec, microsec = divmod(sec,10)
         microsec *= 1e5
-        for i in xrange(2):f.readline()
+        f.readline() #skip one line
+        a = map(int,f.readline().split())
+        smdict['lilax'] = a[-4]
+        smdict['compdir'] = a[-3]
         a = map(int,f.readline().split())
         smdict['prepend'] = a[1]
         smdict['append'] = a[2]
         nptsa = a[3]
         nptsv = a[4]
         nptsd = a[5]
-        listnpts = [nptsa,nptsv,nptsd]        
-        for i in xrange(2): f.readline()
+        listnpts = [nptsa,nptsv,nptsd]
+        # read floating-point header        
+        for i in xrange(2): f.readline() #skip two lines
         a = map(float,f.readline().split())
         delta = a[5]
-        for i in xrange(3): f.readline()
+        for i in xrange(3): f.readline() #skip three lines
         st = Stream()
         for i,npts in enumerate(listnpts):
             if npts < 1: break
@@ -67,39 +76,39 @@ class Vol12Reader(object):
                      'starttime': UTCDateTime(year, month, day, hour, minute, nsec, int(microsec)),
                      'npts': npts, 'calib': 1.0,
                      'sampling_rate': 1./delta, 'channel': comp,'smdict':smdict}
-            cnt = 0
-            data = []
-            intv = 8
-            while cnt < npts:
-                a = f.readline()
-                a = a.rstrip()
-                old = 0
-                while True:
-                    val = a[old:old+intv]
-                    if not val: break
-                    if float(val) > 999999.:
-                        data.append(0.)
-                    else:
-                        data.append(float(val))
-                    cnt += 1
-                    old += intv
-            if cnt != npts: 
-                raise Vol12Error("Number of samples in header inconsistent with number of samples in trace: %s"%self.fn)
-            data = np.array(data)
-            st.append(Trace(data,stats))
+            if self.headonly:
+                # number of full lines
+                nl = npts/10
+                # number of remaining entries in last line
+                nr = npts%10
+                # one full line contains 80 characters plus the end-of-line character
+                f.read(nl*(10*8+1))
+                f.read(nr*8+1)
+                st.append(Trace(header=stats))
+            else:
+                cnt = 0
+                data = []
+                intv = 8
+                while cnt < npts:
+                    a = f.readline()
+                    a = a.rstrip()
+                    old = 0
+                    while True:
+                        val = a[old:old+intv]
+                        if not val: break
+                        if float(val) > 999999.:
+                            data.append(0.)
+                        else:
+                            data.append(float(val))
+                        cnt += 1
+                        old += intv
+                if cnt != npts: 
+                    raise Vol12Error("Number of samples in header inconsistent with number of samples in trace: %s"%self.fn)
+                data = np.array(data)
+                st.append(Trace(data,stats))
                 
         return st
 
 if __name__ == '__main__':
-    import shutil
-    dirdat = u'H:\workspace\svn.obspy.org\\branches\strong_motion'
-    testfn = os.path.join(dirdat, 'O90400A1.V1A')
-    testfn = os.path.join(dirdat,'19900210032700E_Vol2_O90400A1.V2A')
-    #testfn = r's:\proc\2011\06_Prelim\2011-06-13_022049\Vol1\data\20110613_022049_CACS.v1a'
-    testfn = '20110613_022049_CACS.v1a'
-    #shutil.copy(testfn,dirdat)
-    v1 = Vol12Reader(testfn)
-    print v1.stream
-    v1.stream.plot() 
-    
+    pass    
         
